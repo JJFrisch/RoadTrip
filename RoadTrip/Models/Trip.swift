7210880a08bd935e45883bc20fb0e745a9843668
Jake Frischmann <JJFrisch@outlook.com>
2026-01-05 13:24:16 -0500

    refactor: remove days property from Trip model to streamline data handling

--- RoadTrip/Models/Trip.swift
+++ RoadTrip/Models/Trip.swift
@@ -44,9 +44,6 @@ class Trip {
     var isShared: Bool = false // Whether trip is shared with others
     var lastSyncedAt: Date? // Last time synced to cloud
     var cloudId: String? // ID in cloud database for sync
-
-    @Relationship(deleteRule: .cascade, inverse: \TripDay.trip)
-    var days: [TripDay]?
     
     init(name: String, startDate: Date, endDate: Date) {
         self.name = name
